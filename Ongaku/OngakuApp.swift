4d3e15c8116dfceae8c580806cdd57764d992e56
potter-18 <ankityadaav13@gmail.com>
2025-03-09 11:31:21 +0530

    Initial Commit

---
+++ Ongaku/OngakuApp.swift
@@ -0,0 +1,17 @@
+//
+//  OngakuApp.swift
+//  Ongaku
+//
+//  Created by Ankit Yadav on 09/03/25.
+//
+
+import SwiftUI
+
+@main
+struct OngakuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OngakuTests/OngakuTests.swift
@@ -0,0 +1,17 @@
+//
+//  OngakuTests.swift
+//  OngakuTests
+//
+//  Created by Ankit Yadav on 09/03/25.
+//
+
+import Testing
+@testable import Ongaku
+
+struct OngakuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
